cdb8d52f8fddecc8678106f03545c68f8446914e
MinhazAliKhan <minhazjohny@gmail.com>
2024-07-09 17:21:37 -0500

    first commit

---
+++ index1.js
@@ -0,0 +1,21 @@
+// let text = "java Script";
+// let strArr = Array.from(text);
+// // console.log(strArr);
+// let strMap = strArr
+//   .map((curElem, index) => {
+//     return `${curElem}-${index}`;
+//   })
+//   .filter((elem) => {
+//     return elem !== null;
+//   });
+// console.log(strMap);
+// let str = "I am Minhaz";
+// // console.log(str.indexOf("Minhaz"));
+// let arr = Array.from(str);
+// // console.log(arr);
+// let arrMap = arr.map((curElem, index) => `${curElem}-${index}`);
+// console.log(arrMap);
+let str = "I Love Java Script. I am Java Script Developer";
+
+console.log(str.lastIndexOf("Java Script"));
+console.log(str.indexOf("Java Script"));
