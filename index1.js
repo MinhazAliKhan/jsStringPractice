caf5d1e2f73b776d423b94349b720b4fba9e162c
MinhazAliKhan <minhazjohny@gmail.com>
2024-07-09 18:10:17 -0500

    second commit

--- index1.js
+++ index1.js
@@ -17,5 +17,19 @@
 // console.log(arrMap);
 let str = "I Love Java Script. I am Java Script Developer";
 
-console.log(str.lastIndexOf("Java Script"));
-console.log(str.indexOf("Java Script"));
+// console.log(str.lastIndexOf("Java Script"));
+// console.log(str.indexOf("Java Script"));
+// console.log(str.search(/java Script/i));
+// console.log(str.match("Java Script"));
+// console.log(str.match(/Java Script/g));
+// console.log(str.match(/Java script/gi));
+// let value = str.matchAll("Java Script");
+// console.log(...value);
+// for (let item of value) {
+//   console.log(item);
+//   console.log(item[0]);
+//   console.log(item.input);
+//   console.log(item.index);
+// }
+
+console.log(str.includes("Java Script"));
