49b7d6119315f78798015eb84293f352a3ba51ed
MinhazAliKhan <minhazjohny@gmail.com>
2024-07-11 00:53:52 -0500

    expression Practice

---
+++ datatype2.js
@@ -0,0 +1,11 @@
+console.log(10 + "20");
+console.log(9 - "5");
+console.log("Java" + "Script");
+console.log(" " + " ");
+let sum = " " + 0;
+console.log(typeof sum);
+console.log("vinod" - "thapa");
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
---
+++ expression.js
@@ -0,0 +1,17 @@
+// var age = 18;
+// var hadDrivingLicence = true;
+// console.log(age >= 18 && hadDrivingLicence);
+// console.log(-"3");
+// var x = 5;
+// var y = --x;
+// console.log(y);
+// console.log(x);
+// var x = 5;
+// var y = x++;
+// console.log(y);
+// console.log(x);
+console.log(typeof ("5" - 3));
+
+console.log(2 < 12 < 5);
+
+console.log("20" + 10 + 10);
