4d35669f105286ab3c271f091fdc979a5a0a114c
MinhazAliKhan <minhazjohny@gmail.com>
2024-07-11 00:23:57 -0500

    dataType practice

---
+++ index.js
@@ -0,0 +1,36 @@
+// null data type
+// var a = null;
+// console.log(a);
+// console.log(typeof a);
+// var a = "10";
+// console.log(a);
+// console.log(typeof a);
+// console.log(Number(a));
+// console.log(typeof a);
+// var b = "5";
+
+// console.log(typeof +b);
+// var str = 5;
+
+// console.log(typeof (str + ""));
+// var str1 = 7;
+// newstr = String(str1);
+// console.log(typeof newstr);
+//? Truthy values are treated as true when used in conditions. Examples include:
+// 👉 true
+// 👉 Any non-empty string ("hello")
+// 👉 Any non-zero number (42)
+// 👉 Arrays and objects, even if they're not empty
+
+// Falsy values are treated as false in boolean contexts. Examples include:
+// 👉 false
+// 👉 0 (zero)
+// 👉 '' (an empty string)
+// 👉 null
+// 👉 undefined
+// 👉 NaN (Not a Number)
+let number = 34.2;
+console.log(parseInt(number));
+console.log(parseFloat(number));
+console.log(isNaN("vinod"));
+console.log(isNaN(5));
--- index1.js
+++ index1.js
@@ -15,7 +15,7 @@
 // // console.log(arr);
 // let arrMap = arr.map((curElem, index) => `${curElem}-${index}`);
 // console.log(arrMap);
-let str = "I Love Java Script. I am Java Script Developer";
+// let str = "I Love Java Script. I am Java Script Developer";
 
 // console.log(str.lastIndexOf("Java Script"));
 // console.log(str.indexOf("Java Script"));
@@ -32,4 +32,26 @@ let str = "I Love Java Script. I am Java Script Developer";
 //   console.log(item.index);
 // }
 
-console.log(str.includes("Java Script"));
+// console.log(str.includes("Java Script"));
+
+// let str = "Minhaz Ali Khan Minhaz";
+
+// let newStr = str.slice(3, 7);
+// let new1str = str.substring(3, 7);
+// let new1str = str.substring(-4);
+// let new1str = str.slice(-4);
+// let new1str = str.replace("Ali", "Johny");
+// let new1str = str.replaceAll("Minhaz", "Johny");
+
+// console.log(newStr);
+// console.log(new1str);
+// console.log(str.charAt(4));
+// console.log(str.charCodeAt(4));
+// let str = "apple,orange,grape";
+// console.log(str.split(",").reverse().join());
+// console.log(str.split(""));
+let a = ["apple", "ball", "cat"];
+let str = a.join();
+console.log(str);
+console.log(str.split(","));
+console.log(Array.from(str));
