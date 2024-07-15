5ca152a5183540608a14a2b975414924b33eaae9
MinhazAliKhan <minhazjohny@gmail.com>
2024-07-15 17:42:32 -0500

    learn Math Object

--- string.js
+++ string.js
@@ -3,14 +3,41 @@
 // console.log(text.indexOf("minhaz"));
 // console.log(text.lastIndexOf("Minhaz"));
 // console.log(text.lastIndexOf("minhaz"));
-let str = "Minhaz Ali";
-let strArr = Array.from(str);
+// let str = "Minhaz Ali";
+// let strArr = Array.from(str);
 // console.log(strArr);
 // for (let item of strArr) {
 //   console.log(item);
 // }
 // strArr.forEach((item) => console.log(item));
-let newItem = strArr.map((item, index) => {
-  return `${item} - ${index}`;
-});
-console.log(newItem);
+// let newItem = strArr.map((item, index) => {
+//   return `${item} - ${index}`;
+// });
+// console.log(newItem);
+// let str = "I liva JavaScript. I am learning JavaScript";
+// let result = str.search(/javaScript/i);
+// console.log(result);
+// let str = "I liva JavaScript. I am learning JavaScript";
+// let result = str.match("JavaScript");
+// console.log(result);
+// for (let item in result) {
+//   console.log(item);
+// }
+// let str = "I liva JavaScript. I am learning JavaScript";
+// let result = str.match(/javaScript/gi);
+// console.log(result);
+// let str = "I liva JavaScript. I am learning JavaScript";
+// let result = str.matchAll("JavaScript");
+// console.log(result);
+// for (let item of result) {
+//   console.log(item.input);
+// }
+// let str = "I am Minhaz . I am a fulstack developer";
+// let newStr = str.slice(5);
+// let lastIndex = str.lastIndexOf("r");
+// let newStr1 = str.slice(14, lastIndex);
+// console.log(newStr1);
+
+// console.log(newStr);
+let student = ["johny", "minhaz", "ali"];
+console.log(student.reverse().join());
