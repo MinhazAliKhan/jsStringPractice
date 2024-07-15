06ea4c3992b47bbd5e6ec1990bcb36bcc8be2d03
MinhazAliKhan <minhazjohny@gmail.com>
2024-07-15 00:33:41 -0500

    Learn Array crud

---
+++ arrayCrud.js
@@ -0,0 +1,19 @@
+// let persons = ["Ram", "Sita", "john", "tailor"];
+// persons.push("Minhaz");
+// console.log(persons);
+// console.log(persons.push("Johny"));
+// console.log(persons);
+// persons.unshift("Johny");
+// console.log(persons);
+// persons.pop();
+// console.log(persons);
+// persons.shift();
+// console.log(persons);
+// let students = ["Minhaz", "Johny", "Monty", "Ali", "Samuel"];
+// console.log(student);
+// console.log(students);
+// students.splice(3, 1, "Korim");
+// students.splice(3, 2, "Korim", "Anis");
+// students.splice(students.length, 0, "Json");
+
+// console.log(students);
